35d92abc0231962cb4286e1572c517049a617021
Pokoknyabagus <bagussugiono90@gmail.com>
2023-10-29 16:02:35 +0700

    upload semua file web In The Coffee

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+// window.onscroll = function() {
+//     const navBar = document.querySelector('#navBar');
+//     const fixedNav = navBar.offsetTop;
+
+//     if (window.screenY > fixedNav) {    // alias pageYOffset dari screenY
+//         navBar.classList.add('bayangan')
+//     } else {
+//         navBar.classList.remove('bayangan')
+//     }
+// }
+
+
